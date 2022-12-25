cb154202e2feea49f3448e1a176396ed9de4f6a5
pengd-ksu <pengd@ksu.edu>
2022-12-25 00:00:41 -0500

    Finish exercise 2.1

---
+++ gopl.io-master/ch2/exCh2/tempconv_test.go
@@ -0,0 +1,26 @@
+package tempconv
+
+// Test case from:
+// https://github.com/kdama/gopl/blob/master/ch02/ex01/tempconv_test.go
+import (
+	"testing"
+)
+
+func TestKelvinString(t *testing.T) {
+	var tests = []struct {
+		kelvin Kelvin
+		want   string
+	}{
+		{0, "0K"},
+		{1, "1K"},
+		{2.34, "2.34K"},
+		{-5, "-5K"},
+		{-6.78, "-6.78K"},
+	}
+
+	for _, test := range tests {
+		if got := test.kelvin.String(); got != test.want {
+			t.Errorf("Kelvin(%f).String() = %s, want %s", test.kelvin, got, test.want)
+		}
+	}
+}
